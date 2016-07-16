ed955d6075edf950e722b9c71e85a08d50d7e23c
John Crooks <johnmcrooks@gmail.com>
2016-07-16 17:55:16 -0400

    Bare functionality added, hello world posting to twitter

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,8 +1,16 @@
 package com.crooks;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
 
+        Status status = twitter.updateStatus("Hello World! testing, testing, testies, 1, 2 ...3?");
+        System.out.println("done");
     }
 }
