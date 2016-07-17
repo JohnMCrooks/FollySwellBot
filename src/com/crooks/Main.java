1170fbd816f6b7eff6b3e9b9f2b1fdb5aefbda4c
John Crooks <johnmcrooks@gmail.com>
2016-07-16 20:53:15 -0400

    Added Queries and started exploring jsoup to Figure out how to scrape a website for data.

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,16 +1,31 @@
 package com.crooks;
 
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import twitter4j.*;
+
+import java.io.IOException;
+
 
 public class Main {
 
-    public static void main(String[] args) throws TwitterException {
+    public static void main(String[] args) throws TwitterException, IOException {
         Twitter twitter = TwitterFactory.getSingleton();
+        //Status status = twitter.updateStatus("Hello World! testing, testing, testies, 1, 2 ...3?");
+        Query query = new Query("FollyBeach");
+        QueryResult results = twitter.search((query));
 
-        Status status = twitter.updateStatus("Hello World! testing, testing, testies, 1, 2 ...3?");
-        System.out.println("done");
+        for(Status tweet : results.getTweets()){
+            System.out.println(tweet.getUser().getScreenName() + ":" + tweet.getText() );
+        }
+
+
+        Document doc = Jsoup.connect("http://www.surfline.com/surf-report/folly-beach-pier-southside-southeast_5294/").get();
+        Element content = doc.getElementById("current-surf-range");
+
+
+        System.out.println(content);
     }
 }
